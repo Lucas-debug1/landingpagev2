780a7ead2145d90c75119d59274561491aa13f37
Lucas Vilarim <lucasvilarim90@gmail.com>
2026-04-18 11:17:03 -0300

    up index v2

--- script.js
+++ script.js
@@ -5,5 +5,11 @@ function scrollToForm() {
 document.getElementById('leadForm').addEventListener('submit', function(e) {
   e.preventDefault();
 
+  var recaptchaResponse = grecaptcha.getResponse();
+  if (recaptchaResponse.length === 0) {
+    alert('Por favor, complete o reCAPTCHA.');
+    return;
+  }
+
   alert('🔥 Sucesso! Em breve entraremos em contato.');
 });
